636b7a44c0541a54344d2bdcda101f4fc446fecc
Christian Brassat <christian@brassat.eu>
2019-09-23 23:12:11 +0200

    Add color picker

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PageTitle from "./modules/PageTitle";
 import LinkList from "./modules/LinkList";
+import ColorPicker from "./modules/ColorPicker";
 
 const App = () => {
   const [editable, setEditable] = React.useState(false);
@@ -9,6 +10,11 @@ const App = () => {
     <div className={editable ? "--editable" : ""}>
       <PageTitle />
       <LinkList />
+
+      <div className="settingsBar">
+        <ColorPicker property="--colorBackground" label="Background" />
+        <ColorPicker property="--colorForeground" label="Foreground" />
+      </div>
 
       <button id="editButton" onClick={event => setEditable(!editable)}>
         <i className="material-icons">{editable ? "check" : "settings"}</i>
---
+++ src/modules/ColorPicker/index.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { SketchPicker } from "react-color";
+
+class ColorPicker extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { color: "", editable: false };
+  }
+
+  storeColor = () => {
+    localStorage.setItem(
+      "color" + this.props.property,
+      JSON.stringify(this.state.color)
+    );
+  };
+
+  componentDidMount = () => {
+    const color = JSON.parse(
+      localStorage.getItem("color" + this.props.property)
+    );
+
+    if (color) {
+      this.setState({ color: color });
+      this.setProperty(color);
+    }
+  };
+
+  setProperty = color => {
+    document.documentElement.style.setProperty(
+      this.props.property,
+      Object.values(color).join()
+    );
+  };
+
+  handleChangeComplete = value => {
+    const color = { r: value.rgb.r, g: value.rgb.g, b: value.rgb.b };
+    this.setState({ color: color });
+    this.setProperty(color);
+    this.storeColor();
+  };
+
+  handleClick = () => {
+    this.setState({ editable: !this.state.editable });
+  };
+
+  render() {
+    return (
+      <div
+        className={
+          "colorpicker " +
+          this.props.property +
+          (this.state.editable ? " --editable" : "")
+        }
+      >
+        <SketchPicker
+          color={this.state.color}
+          onChangeComplete={this.handleChangeComplete}
+        />
+
+        <div className="colorpicker__preview" onClick={this.handleClick} />
+        {this.props.label}
+      </div>
+    );
+  }
+}
+
+export default ColorPicker;
